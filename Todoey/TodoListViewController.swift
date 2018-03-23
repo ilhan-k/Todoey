5446bf2fcd9dd78c7340e929a34bffb33433c0ac
= <=>
2018-03-23 07:46:59 +0300

    barButtonItemPressed() method added

--- Todoey/TodoListViewController.swift
+++ Todoey/TodoListViewController.swift
@@ -12,7 +12,7 @@ class TodoListViewController: UITableViewController{
 
     @IBOutlet var mainTableView: UITableView!
 
-    let itemArray = ["Find Mike", "Buy Eggos", "Destroy Demogorgon"]
+    var itemArray = ["Find Mike", "Buy Eggos", "Destroy Demogorgon"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -38,5 +38,29 @@ class TodoListViewController: UITableViewController{
         }
         tableView.deselectRow(at: indexPath, animated: true)
     }
+    
+    //MARK: - Add new items
+    @IBAction func barButtonItemPressed(_ sender: UIBarButtonItem) {
+        
+        var textField = UITextField()
+        
+        let alert = UIAlertController(title: "Add New Todoey Item", message: "", preferredStyle: .alert)
+        let action = UIAlertAction(title: "Add Item", style: .default) { (action) in
+            //print(textField.text!)
+            if let itemText = textField.text{
+                self.itemArray.append(itemText)
+                self.tableView.reloadData()
+            }
+        }
+        
+        alert.addTextField { (alertTextField) in
+            alertTextField.placeholder = "New Item"
+            textField = alertTextField
+        }
+        
+        alert.addAction(action)
+        present(alert, animated: true, completion: nil)
+    }
+    
 }
 
