87117491a748360db83ed20b444b02911ad90ef1
= <=>
2018-03-23 21:57:09 +0300

    Item.swift file and class created, File structure changed

---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,14 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by ilhan korkmaz on 23.03.2018.
+//  Copyright © 2018 ilhan korkmaz. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title : String = ""
+    var isDone : Bool = false
+}
